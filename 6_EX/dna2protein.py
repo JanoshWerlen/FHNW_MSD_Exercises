96238b835b1b516607dad28222a862aba1c12068
JanoshWerlen <werleja1@students.zhaw.ch>
2026-06-28 13:20:13 +0200

    updates Repo for submission

--- 6_EX/dna2protein.py
+++ 6_EX/dna2protein.py
@@ -1,12 +1,12 @@
-import random
 from Bio.Seq import Seq
 
+
 class Sequence:
-    def __init__(self, sequence):
-        self.sequence = sequence
+    def __init__(self, value):
+        self.value = value
 
     def __str__(self):
-        return self.sequence
+        return self.value
 
 
 class DNASequence(Sequence):
@@ -16,96 +16,45 @@ class DNASequence(Sequence):
 class ProteinSequence(Sequence):
     pass
 
-class SequenceStorage:
 
+class SequenceUtility:
+    @staticmethod
+    def transcribe_dna_to_rna(dna_sequence):
+        return dna_sequence.replace("T", "U")
+
+    @staticmethod
+    def translate_rna_to_protein(rna_sequence):
+        return str(Seq(rna_sequence).translate())
+
+
+class SequenceStorage:
     _instance = None
-    #Use Singleton
+
     def __new__(cls):
         if cls._instance is None:
-            cls._instance = super(SequenceStorage, cls).__new__(cls)
+            cls._instance = super().__new__(cls)
             cls._instance.data = {}
         return cls._instance
 
-    def save(self, name, seq):
-        self.data[name] = seq
+    def save(self, name, sequence):
+        self.data[name] = sequence
 
     def read(self, name):
         return self.data[name]
 
 
-class DNASequenceGenerator:
-    alphabet = ['A', 'C', 'G', 'T']
-
-    def create_sequence(self, n):
-        result = ''
-        for i in range(n):
-            idx = random.randint(0, 3)
-            result += DNASequenceGenerator.alphabet[idx]
-        return result
-
-
-class Translator:
-
-    @staticmethod
-    def transcribe_dna_to_rna(dna):
-        return dna.replace("T", "U")
-
-    @staticmethod
-    def reverse_transcribe_rna_to_dna(rna):
-        return rna.replace("U", "T")
-
-    @staticmethod
-    def translate_rna_to_protein(rna):
-        rna_seq = Seq(rna)
-        return str(rna_seq.translate())
-
-    @staticmethod
-    def translate_dna_to_protein(dna):
-        dna_seq = Seq(dna)
-        return str(dna_seq.translate())
-
 class SequenceFactory:
-
-    DNA_ALPHABET = ['A', 'C', 'G', 'T']
-
-    PROTEIN_ALPHABET = [
-        'A', 'R', 'N', 'D', 'C',
-        'Q', 'E', 'G', 'H', 'I',
-        'L', 'K', 'M', 'F', 'P',
-        'S', 'T', 'W', 'Y', 'V'
-    ]
-
     @staticmethod
-    def create_sequence(length, seq_type="DNA"):
+    def create_sequence(sequence, sequence_type):
+        if sequence_type.upper() == "DNA":
+            return DNASequence(sequence)
+        if sequence_type.upper() == "PROTEIN":
+            return ProteinSequence(sequence)
+        raise ValueError("sequence_type must be DNA or PROTEIN")
 
-        result = ""
 
-        if seq_type.upper() == "DNA":
-            alphabet = SequenceFactory.DNA_ALPHABET
-
-        elif seq_type.upper() == "PROTEIN":
-            alphabet = SequenceFactory.PROTEIN_ALPHABET
-
-        else:
-            raise ValueError("Unknown sequence type")
-
-        for _ in range(length):
-            result += random.choice(alphabet)
-
-        if seq_type.upper() == "DNA":
-            return DNASequence(result)
-
-        else:
-            return ProteinSequence(result)
-
-if __name__ == '__main__':
-
-    dna_sequence = SequenceFactory.create_sequence(50, "DNA")
-
-    print("Random DNA Sequence:")
-    print(dna_sequence)
-
-    cd28 = (
+if __name__ == "__main__":
+    cd28_dna = (
         "ATGCTCAGGCTGCTCTTGGCTCTCAACTTATTCCCTTCAATTCAAGTAACAGGAAACAAGATTTTGGTGA"
         "AGCAGTCGCCCATGCTTGTAGCGTACGACAATGCGGTCAACCTTAGCTGGAAACACCTTTGTCCAAGTCC"
         "CCTATTTCCCGGACCTTCTAAGCCCTTTTGGGTGCTGGTGGTGGTTGGTGGAGTCCTGGCTTGCTATAGC"
@@ -114,18 +63,20 @@ if __name__ == '__main__':
         "CTTCGCAGCCTATCGCTCCTGA"
     )
 
-    rna_sequence = Translator.transcribe_dna_to_rna(cd28)
-
-    print("\nRNA Sequence:")
-    print(rna_sequence)
-
-    protein_sequence = Translator.translate_rna_to_protein(rna_sequence)
-
-    print("\nProtein Sequence:")
-    print(protein_sequence)
+    dna_sequence = SequenceFactory.create_sequence(cd28_dna, "DNA")
+    rna_sequence = SequenceUtility.transcribe_dna_to_rna(dna_sequence.value)
+    protein_value = SequenceUtility.translate_rna_to_protein(rna_sequence)
+    protein_sequence = SequenceFactory.create_sequence(protein_value, "PROTEIN")
 
     storage = SequenceStorage()
-
-    storage.save("CD28_DNA", cd28)
-    storage.save("CD28_RNA", rna_sequence)
+    storage.save("CD28_DNA", dna_sequence)
+    storage.save("CD28_RNA", Sequence(rna_sequence))
     storage.save("CD28_PROTEIN", protein_sequence)
+
+    print("Singleton pattern is used for SequenceStorage.")
+    print("CD28 DNA:")
+    print(storage.read("CD28_DNA"))
+    print("\nCD28 RNA:")
+    print(storage.read("CD28_RNA"))
+    print("\nCD28 protein:")
+    print(storage.read("CD28_PROTEIN"))
